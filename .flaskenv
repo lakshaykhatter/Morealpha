61bfaf1e7c7f939651693f81929ff7e06f91179f
Lakshay Khatter <lakshaykhatter@Lakshays-MacBook-Pro.local>
2019-04-01 13:47:21 -0400

    Created inital app, with login, logout, ticker, and posts

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=MoreAlpha.py
+export FLASK_DEBUG=1
---
+++ app/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+from app.models import User
+from app import db
+
+
+class LoginForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(message="Please enter your username")])
+	password = PasswordField('Password', validators=[DataRequired(message="Please enter your password")])
+	remember_me = BooleanField('Remember Me')
+	submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired()])
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different username.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+	first_name = StringField('First Name', validators=[DataRequired()])
+	last_name = StringField('Last Name', validators=[DataRequired()])
+	about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+	submit = SubmitField('Submit')
