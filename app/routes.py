f4df4d1239d3d9c92da07e138ef56cb4750f2a9d
Lakshay Khatter <lakshaykhatter@Lakshays-MacBook-Pro.local>
2019-04-05 11:23:14 -0400

    Created index page template, fixed users page

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,9 @@ import os
 @app.route('/index')
 def index():
 	posts = Post.query.order_by('timestamp').limit(10)
-	return render_template("index.html", posts=posts)
+	sectors = Ticker.query.with_entities(Ticker.sector).distinct().all()
+
+	return render_template("index.html", posts=posts, sectors=sectors )
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -33,6 +35,7 @@ def login():
 			next_page = url_for('index')
 		return redirect(next_page)
 	return render_template('login.html', title='Sign In', form=form)
+
 
 @app.route('/logout')
 @login_required
@@ -54,7 +57,6 @@ def save_picture(form_picture):
 	return picture_fn
 
 
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if current_user.is_authenticated:
@@ -63,11 +65,9 @@ def register():
 	if form.validate_on_submit():
 		user = User(username=form.username.data, email=form.email.data, 
 				first_name=form.first_name.data, last_name=form.last_name.data)
-
 		if form.picture.data:
 			picture_file = save_picture(form.picture.data)
 			user.image_file = picture_file
-
 		user.set_password(form.password.data)
 		db.session.add(user)
 		db.session.commit()
@@ -79,13 +79,14 @@ def register():
 @login_required
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = Post.query.filter_by(user_id=user.id)
-	tickers = Ticker.query.get(1)
+	posts = Post.query.filter_by(user_id=user.id).limit(5).all()
+	posts = None if posts == [] else posts
+	tickers = Ticker.query.all()[0:50]
+
 	if user.image_file:
 		image_file = url_for('static', filename='profile_pics/' + user.image_file)
 	else:
 		image_file = url_for('static', filename='profile_pics/default.png')
-
 	return render_template('user.html', user=user, posts=posts, tickers=tickers, image_file=image_file)
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -105,9 +106,6 @@ def edit_profile():
 		form.last_name.data = current_user.last_name
 
 	return render_template('edit_profile.html', title='Edit Profile', form=form)
-
-
-
 
 
 @app.route('/ticker/<ticker>')
