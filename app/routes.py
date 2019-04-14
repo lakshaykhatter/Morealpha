f95de9897dab0bed3723140f68bd007be21a5136
Lakshay Khatter <lakshaykhatter@Lakshays-MacBook-Pro.local>
2019-04-14 14:59:37 -0400

    fixed the index page likes onto one line

--- app/routes.py
+++ app/routes.py
@@ -204,14 +204,11 @@ def like_action(post_id, action):
 def followuser(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
-        flash('You cannot follow yourself!')
         return redirect(url_for('user', username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are following {}!'.format(username))
     return redirect(url_for('user', username=username))
 
 @app.route('/unfollowuser/<username>')
@@ -219,10 +216,8 @@ def followuser(username):
 def unfollowuser(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found.'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
-        flash('You cannot unfollow yourself!')
         return redirect(url_for('user', username=username))
     current_user.unfollow(user)
     db.session.commit()
